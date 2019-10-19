03800d39ec8db64346a7b4ccfeede039e2a6aab9
suet <styinglee@gmail.com>
2019-10-19 13:49:17 +0000

    drawOrbit

--- static/universe.js
+++ static/universe.js
@@ -31,6 +31,23 @@ function drawObject(object) {
   ctx.fill();
 }
 
+function drawOrbit(object) {
+  var ppos = object.parent.position;
+  var x_diff = object.position.x-ppos.x;
+  var y_diff = object.position.y-ppos.y;
+  var radius = Math.sqrt(x_diff*x_diff + y_diff*y_diff);
+  ctx.beginPath();
+  ctx.arc(
+    ppos.x,
+    ppos.y,
+    radius,
+    0,
+    2 * Math.PI
+  );
+  ctx.strokeStyle = '#424242';
+  ctx.stroke();
+}
+
 let sun = {
   position: { x: canvas.width / 2, y: canvas.height / 2 },
   type: "star",
@@ -38,11 +55,21 @@ let sun = {
   size: 20
 };
 
+let p1 = {
+    parent: sun,
+    position: { x: sun.position.x+180, y: sun.position.y+120 },
+    type: "star",
+    color: "blue",
+    size: 13
+}
+
 init();
 drawObject(sun);
-draw(canvas.width / 3, canvas.height / 2, 3, { color: "cyan" });
-draw(canvas.width / 4, canvas.height / 2, 2, { color: "red" });
-draw(canvas.width / 5, canvas.height / 2, 5, { color: "brown" });
+drawOrbit(p1);
+drawObject(p1);
+// draw(canvas.width / 3, canvas.height / 2, 3, { color: "cyan" });
+// draw(canvas.width / 4, canvas.height / 2, 2, { color: "red" });
+// draw(canvas.width / 5, canvas.height / 2, 5, { color: "brown" });
 
 /*
 function expand() {
