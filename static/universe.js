3a9df9d2a2664ddc37850080517668ef9ca6d981
Mustafa Abdeltawwab <mustafajnr@gmail.com>
2019-10-19 16:13:04 +0100

    refactoring to object oriented JS

--- static/universe.js
+++ static/universe.js
@@ -4,6 +4,60 @@ canvas.height = window.innerHeight;
 
 const ctx = canvas.getContext("2d");
 
+function Universe() {
+    this.objects = new Array();
+    this.addObject = function(object) {
+        this.objects.push(object);
+    };
+    this.draw = function(context) {
+        for (i=0; i<this.objects.length; i++) {
+            this.objects[i].draw(context);
+        }
+    };
+};
+
+function getColor(attribs) {
+    if ("color" in attribs) {
+        return attribs["color"];
+    }
+    return "green";
+}
+
+function drawOrbit(ctx, pos, radius) {
+    ctx.beginPath();
+    ctx.arc(pos.x, pos.y, radius, 0, 2*Math.PI);
+    ctx.strokeStyle = '#424242';
+    ctx.stroke();
+}
+
+function CelestialObject(type, size, pos, attribs) {
+    this.type = type;
+    this.pos = pos;
+    this.size = size;
+    this.attribs = attribs;
+    this.draw = function(ctx) {
+        if ("parent" in this.attribs)
+        {
+            let x_diff = this.pos.x - this.attribs.parent.pos.x;
+            let y_diff = this.pos.y - this.attribs.parent.pos.y;
+            let radius = Math.sqrt(x_diff*x_diff+y_diff*y_diff);
+            drawOrbit(ctx, this.attribs.parent.pos, radius);
+        }
+        ctx.fillStyle = getColor(this.attribs);
+        ctx.beginPath();
+        ctx.arc(
+            this.pos.x,
+            this.pos.y,
+            this.size / 2,
+            0,
+            2 * Math.PI
+        );
+        ctx.fill();
+    }
+}
+
+let universe = new Universe();
+
 function init() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = "black";
@@ -11,65 +65,28 @@ function init() {
   ctx.fill();
 }
 
-function draw(x, y, r, properties) {
-  ctx.fillStyle = properties.color;
-  ctx.beginPath();
-  ctx.arc(x, y, r, 0, 2 * Math.PI);
-  ctx.fill();
-}
-
-function drawObject(object) {
-  ctx.fillStyle = object.color;
-  ctx.beginPath();
-  ctx.arc(
-    object.position.x,
-    object.position.y,
-    object.size / 2,
-    0,
-    2 * Math.PI
-  );
-  ctx.fill();
-}
-
-function drawOrbit(object) {
-  var ppos = object.parent.position;
-  var x_diff = object.position.x-ppos.x;
-  var y_diff = object.position.y-ppos.y;
-  var radius = Math.sqrt(x_diff*x_diff + y_diff*y_diff);
-  ctx.beginPath();
-  ctx.arc(
-    ppos.x,
-    ppos.y,
-    radius,
-    0,
-    2 * Math.PI
-  );
-  ctx.strokeStyle = '#424242';
-  ctx.stroke();
-}
-
-let sun = {
-  position: { x: canvas.width / 2, y: canvas.height / 2 },
-  type: "star",
-  color: "yellow",
-  size: 20
-};
-
-let p1 = {
-    parent: sun,
-    position: { x: sun.position.x+180, y: sun.position.y+120 },
-    type: "star",
-    color: "blue",
-    size: 13
-}
-
 init();
-drawObject(sun);
-drawOrbit(p1);
-drawObject(p1);
-// draw(canvas.width / 3, canvas.height / 2, 3, { color: "cyan" });
-// draw(canvas.width / 4, canvas.height / 2, 2, { color: "red" });
-// draw(canvas.width / 5, canvas.height / 2, 5, { color: "brown" });
+
+let sun = new CelestialObject("star", 20, {x : canvas.width /2, y: canvas.height / 2}, {
+  color: "yellow"
+});
+
+universe.addObject(sun);
+
+universe.addObject(new CelestialObject("planet", 3, {x: canvas.width / 3, y:canvas.height / 2}, { color: "cyan", parent: sun }));
+
+universe.addObject(new CelestialObject("planet", 2, {x: canvas.width / 4, y:  canvas.height / 2}, { color: "red", parent: sun }));
+
+universe.addObject(new CelestialObject(
+"planet", 5, {x:canvas.width / 5, y: canvas.height / 2}, { color: "brown", parent: sun }));
+
+universe.draw(ctx);
+
+canvas.addEventListener("dblclick", function(e) {
+    universe.addObject(new CelestialObject(
+        "planet", 10, {x: e.x, y: e.y }, {parent: sun}));
+    universe.draw(ctx);
+});
 
 /*
 function expand() {
@@ -81,3 +98,4 @@ function expand() {
 
 window.requestAnimationFrame(expand);
 */
+
